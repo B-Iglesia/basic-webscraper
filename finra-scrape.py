4fc60c399cb50be7b9094af8c5900289fa260ec9
Brad Iglesia <bradiglesia@gmail.com>
2023-02-09 14:25:10 -0800

    figured out how to get all of the CRDS per page. Attempting to go 20 pages

--- finra-scrape.py
+++ finra-scrape.py
@@ -14,6 +14,8 @@ mozilla_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537
 
 url = 'https://brokercheck.finra.org/'
 
+pages = 20
+
 def main():
     options = webdriver.ChromeOptions() 
     options.add_argument('--headless')
@@ -25,13 +27,22 @@ def main():
         search_parent = driver.find_element(By.XPATH, "//div[@class='flex-1 w-full min-w-0 h-10 sm:flex-none sm:mb-4']")
         search_bar = search_parent.find_element(By.XPATH, ".//input")
         search_bar.send_keys("Smith" + "\n")
-        time.sleep(2)
+        time.sleep(1)
         
+        i = 0
+        while i <= pages:
         ## Go through rows crawling spans for info
-        parent_card_elements = driver.find_elements(By.XPATH, "//div[@class='flex flex-col border-t-4 px-4 py-2 h-60 border-gray-70 bg-gray-20']") 
-        for element in parent_card_elements:
-            text = element.find_element(By.XPATH, "//div[@class='text-gray-85 text-left font-semibold mt-1 text-xs ng-star-inserted']//span[2]")
-            print (text.text)
+            parent_element = driver.find_element(By.XPATH, "//div[@class='flex w-full flex-row flex-wrap justify-around gap-3']")
+            parent_cards = parent_element.find_elements(By.XPATH, "//div[@class='mb-2 card-dimensions flex-1 text-center ng-star-inserted']") 
+            print(len(parent_cards))
+            for card in parent_cards:
+                text = card.find_element(By.XPATH, ".//div[@class='text-gray-85 text-left font-semibold mt-1 text-xs ng-star-inserted']//span[2]")
+                print(text.text)
+            next_button = driver.find_element(By.XPATH, "//div[@class='px-2 ng-star-inserted']//button")
+            next_button.click()
+            time.sleep(1)
+            i+=1
+
 
 
 
